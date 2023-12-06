255a94acc6cc82d5a64c781da8030c8417fb26d7
EvgenSakun <evgen.sakun20@gmail.com>
2023-12-06 18:11:24 +0300

    Finished

--- OptionalEnumDz/OptionalEnumDz/ViewController.swift
+++ OptionalEnumDz/OptionalEnumDz/ViewController.swift
@@ -11,7 +11,101 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // 1:
+        optional()
+        
+        func optional(_ aser: Int? = nil) {
+            if let aser {
+                print(aser * 2)
+            } else {
+                print("Значение отсутствует")
+            }
+        }
+        
+        // 2:
+        forceunwrap()
+        
+        func forceunwrap(_ a: String? = nil) {
+            if a != nil {
+                print("Hello, \(a!)")
+            } else {
+                print("Ошибка")
+            }
+        }
+        
+        // 3:
+        guardlet()
+        
+        func guardlet(_ a: Int? = nil) {
+            guard let b = a else {
+                print("Значение отсутствует")
+                return
+            }
+            print(b + 20)
+            print(b * 30)
+            print(b - 15)
+        }
+        
+        // 4:
+        
+        construction("asd")
+        
+        func construction(_ a: String? = nil) {
+            if let b = Int(a!) {
+                print(b)
+            } else {
+                return
+            }
+            
+        }
+        
+        // 5:
+        
+        enumeration()
+        
+        enum Book {
+            case chapterOne(pages: Int)
+            case chapterTwo(pages: Int)
+            case chapterThree(pages: Int)
+        }
+        
+        func enumeration() {
+            let book1 = Book.chapterThree(pages: 128)
+            print(book1)
+        }
+        
+        // 6:
+        let Clothes1 = Clothes.clothesType1.ShirtRed
+        let wardrobe = Clothes.UseClothesType1(type1: Clothes1)
+        wardrobe.getClothes()
+        
+        
+        
+        enum Clothes {
+            enum clothesType1 {
+                case ShirtRed
+                case ShortsBlue
+               
+            }
+            enum clothesType2 {
+                case ShirtBlue
+                case ShortsRed
+                
+            }
+            case UseClothesType1(type1: clothesType1)
+            case UseClothesType2(type2: clothesType2)
+            
+            func getClothes() {
+                switch self {
+                case .UseClothesType1(let type1):
+                    print("Хочу надеть \(type1)")
+                case .UseClothesType2(let type2):
+                    print("Хочу надеть \(type2)")
+                }
+            }
+        }
+        
     }
 
 
