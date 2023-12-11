b0d1630e0bada2a368d9cdaa45a9a0872045aedf
EvgenSakun <evgen.sakun20@gmail.com>
2023-12-11 17:53:48 +0300

    Start

---
+++ MassivDzSakun/MassivDzSakun/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MassivDzSakun
+//
+//  Created by Evgeny Sakun on 11.12.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
